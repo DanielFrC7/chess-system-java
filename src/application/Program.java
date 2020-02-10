7fda2bc1ad9eaab877d3ef86b61b36e56da399ea
Daniel Freitas <skaleno@hotmail.com>
2020-02-10 20:47:02 -0200

    Fist class: position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,11 @@
 package application;
 
-public class Program {
+import boadgame.Position;
 
+public class Program {
+	public static void main(String[]agrs ) {
+		
+		Position pos = new Position(3 , 5);
+		System.out.println(pos);
+	}
 }
