550c9b77376dda847290fc67688e6bd364061f87
Daniel Freitas <skaleno@hotmail.com>
2020-02-18 13:04:39 -0300

    plancing pieces on the board

---
+++ src/chess/pieces/Torre.java
@@ -0,0 +1,16 @@
+package chess.pieces;
+
+import boadgame.Board;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Torre extends ChessPiece {
+
+	public Torre(Board board, Color color) {
+		super(board, color);
+	}
+	@Override
+	public String toString() {
+		return "T";
+	}
+}
